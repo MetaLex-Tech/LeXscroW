86fa9b64216821a24340cb3d2a81a6ca8d81dcc4
merisman <6125373+merisman@users.noreply.github.com>
2024-12-08 23:23:50 -0500

    Update before OZ install

--- script/deploy.s.sol
+++ script/deploy.s.sol
@@ -1,40 +1,44 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity 0.8.18;
+pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
-import {DoubleTokenLexscrow} from "../src/DoubleTokenLexscrow.sol";
+import {DoubleTokenLexscrowFactory} from "../src/DoubleTokenLexscrowFactory.sol";
+import {DoubleTokenLexscrowRegistry} from "RicardianTriplerDoubleTokenLeXscroW/DoubleTokenLexscrowRegistry.sol";
+import {AgreementV1Factory} from "RicardianTriplerDoubleTokenLeXscroW/RicardianTriplerDoubleTokenLexscrow.sol";
 import {console} from "forge-std/console.sol";
 
 bytes32 constant DETERMINISTIC_DEPLOY_SALT = keccak256(abi.encodePacked("metalex-coded"));
+address constant admin = 0x68Ab3F79622cBe74C9683aA54D7E1BBdCAE8003C;
 
 contract Deploy is Script {
     function run() external {
-            deployerAddress = vm.addr(vm.envUint("PRIVATE_KEY_DEPLOY"));
+            address deployerAddress = vm.addr(vm.envUint("PRIVATE_KEY_DEPLOY"));
             uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY_DEPLOY");
 
-             // Create bytecode with constructor arguments
         bytes memory bytecode = abi.encodePacked(
-            type(DoubleTokenLexscrow).creationCode
+            type(DoubleTokenLexscrowFactory).creationCode
         );
-
         address predictedAddress = vm.computeCreate2Address(
-            salt,
+            DETERMINISTIC_DEPLOY_SALT,
             keccak256(bytecode)
         );
-        
         console.log("Predicted address:", predictedAddress);
-
         vm.startBroadcast(deployerPrivateKey);
-                // Calculate the address before deployment
+        DoubleTokenLexscrowFactory doubleTokenLexscrowFactory = new DoubleTokenLexscrowFactory{salt: DETERMINISTIC_DEPLOY_SALT}();
+        console.log("Deployed address:", address(doubleTokenLexscrowFactory));
+        DoubleTokenLexscrowRegistry doubleTokenLexscrowRegistry = new DoubleTokenLexscrowRegistry{salt: DETERMINISTIC_DEPLOY_SALT}(deployerAddress);
+        console.log("Deployed address:", address(doubleTokenLexscrowRegistry));
+        AgreementV1Factory agreementV1Factory = new AgreementV1Factory{salt: DETERMINISTIC_DEPLOY_SALT}(address(doubleTokenLexscrowRegistry));
+        console.log("Deployed address:", address(agreementV1Factory));  
+        doubleTokenLexscrowRegistry.enableFactory(address(agreementV1Factory));
+        //set .25% fee
+        console.log("Current receiver:", doubleTokenLexscrowFactory.receiver());
+        console.log("deployerAddress:", deployerAddress);
+        doubleTokenLexscrowFactory.updateFee(true, 250000);
+        doubleTokenLexscrowFactory.acceptFeeUpdate();
+        doubleTokenLexscrowFactory.updateReceiver(admin);
+        doubleTokenLexscrowRegistry.updateAdmin(admin);
 
-        // Deploy the contract
-        address deployedAddress = vm.create2(
-            0, // value to send
-            salt,
-            bytecode
-        );
-
-        console.log("Deployed address:", deployedAddress);
-            vm.stopBroadcast();
+        vm.stopBroadcast();
     }
 }   
