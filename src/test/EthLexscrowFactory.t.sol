4ed3389a8689dd20fadc44510ae78af20063714a
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2025-05-27 13:00:06 -0400

    Update EthLexscrowFactory.t.sol

--- src/test/EthLexscrowFactory.t.sol
+++ src/test/EthLexscrowFactory.t.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.18;
 
-import "forge-std/Test.sol";
+import "forge-std/src/Test.sol";
 import "src/EthLexscrowFactory.sol";
 
 /// @notice foundry framework testing of EthLexscrowFactory.sol
